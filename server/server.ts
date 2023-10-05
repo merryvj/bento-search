0dcb11a7a936909a637f37f059d845709fb5b0b3
Mary Jiang <mvjiang@gmail.com>
2023-10-05 02:00:27 -0400

    manual middleware

--- server/server.ts
+++ server/server.ts
@@ -12,15 +12,26 @@ const metaphor = new Metaphor(process.env.METAPHOR_API_KEY);
 
 const app = express();
 
-const corsOptions = {
-  origin: ['*'],
-  allowedHeaders: ["Content-Type", "Authorization", "Access-Control-Allow-Methods", "Access-Control-Request-Headers"],
-  credentials: true,
-  enablePreflight: true
-}
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://bento-search.onrender.com"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+  res.setHeader("Access-Control-Max-Age", 7200);
 
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions))
+  next();
+});
 
 // register body-parser middleware
 app.use(bodyParser.json());
