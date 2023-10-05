ccff45887cdfe640d3e12e28521e2617b1557a24
Mary Jiang <mvjiang@gmail.com>
2023-10-04 22:27:22 -0400

    updated puppeteer executable path

--- server/server.ts
+++ server/server.ts
@@ -82,7 +82,18 @@ app.get('/capture', async (req: Request, res: Response) => {
   }
 
   try {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+      args: [
+        "--disable-setuid-sandbox",
+        "--no-sandbox",
+        "--single-process",
+        "--no-zygote",
+      ],
+      executablePath:
+        process.env.NODE_ENV === "production"
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath()
+    });
     const page = await browser.newPage();
     await page.goto(url.toString());
 
